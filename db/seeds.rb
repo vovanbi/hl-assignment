24cea6719419453c4c86d44cc25fb5cf2786e892
vovanbi <vanbi010599@gmail.com>
2024-06-28 11:04:33 +0700

    fix db-seed and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,14 @@ require 'faker'
 puts 'Migration data'
 
 jokes = []
-20.times { |i| jokes << { content: "No. #{i} #{Faker::TvShows::NewGirl.quote * 10}" } }
+
+jokes << { content: 'A child asked his father, "How were people born?" So his father said, "Adam and Eve made babies, then their babies became adults and made babies, and so on."' }
+jokes << { content: 'The child then went to his mother, asked her the same question and she told him, "We were monkeys then we evolved to become like we are now."' }
+jokes << { content: 'The child ran back to his father and said, "You lied to me!" His father replied, "No, your mom was talking about her side of the family."' }
+jokes << { content: 'Teacher: "Kids,what does the chicken give you?" Student: "Meat!" Teacher: "Very good! Now what does the pig give you?" Student: "Bacon!" Teacher: "Great! And what does the fat cow give you?" Student: "Homework!"' }
+jokes << { content: 'The teacher asked Jimmy, "Why is your cat at school today Jimmy?" Jimmy replied crying, "Because I heard my daddy tell my mommy, "I am going to eat that pussy once Jimmy leaves for school today!"' }
+jokes << { content: 'A housewife, an accountant and a lawyer were asked "How much is 2+2?" The housewife replies: "Four!". The accountant says: "I think it\'s either 3 or 4. Let me run those figures through my spreadsheet one more time." The lawyer pulls the drapes, dims the lights and asks in a hushed voice, "How much do you want it to be?"' }
+
 Joke.insert_all(jokes)
 
 puts 'Migration data Joke successful !!!'
